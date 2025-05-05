147f305ffa867deb0ec0daeccb7c97a6a818f620
harshitalamba11 <lambaharshita1@gmail.com>
2025-05-06 01:56:51 +0530

    Authentication complete

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -30,3 +30,28 @@ export const signup=async (req,res)=>{
         res.status(500).json({message:error.message})
     }
 }
+
+
+export const login=async(req,res)=>{
+    try{
+        const {email,password}=req.body;
+        const user=await User.findOne({email});
+        const isMatch=await bcryptjs.compare(password,user.password);
+      if(!user || !isMatch){
+            return res.status(400).json({message:"Invalid credentials"})
+        }else{
+        res.status(200).json({message:"Login successful",
+
+            user:{
+            _id:user._id,
+            Fullname:user.Fullname,
+            email:user.email
+            }
+        })
+        }
+    }
+    catch(error){
+        console.log("Error"+error.message);
+        res.status(500).json({message:error.message})
+    }
+}
